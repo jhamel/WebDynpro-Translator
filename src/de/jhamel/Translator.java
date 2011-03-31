c32b44b21ad133c5083cd13ded0ace90137a4234
jhamel <johannes.hamel@gmail.com>
2011-03-31 22:42:26 +0200

    changes

--- src/de/jhamel/Translator.java
+++ src/de/jhamel/Translator.java
@@ -7,8 +7,8 @@ public class Translator {
     private static final String BASEDIR = "C:\\Dokumente und Einstellungen\\J19727\\.dtc\\2\\DCs\\eonis.com\\eea\\bpexd\\wd";
 
     public void doMagic() throws Exception {
-        CsvReader csvReader = new CsvReader("translation_20110330_VDE.csv", new GermanZeroEnglishOne());
-        csvReader.readFile();
+        CsvReader csvReader = new CsvReader(new GermanZeroEnglishOne());
+        csvReader.readFile("translation_20110330_VDE.csv");
     }
 
     public static void main(String[] args) throws Exception {
--- src/de/jhamel/csv/CsvReader.java
+++ src/de/jhamel/csv/CsvReader.java
@@ -9,16 +9,14 @@ public class CsvReader {
 
     private static Logger log = Logger.getLogger(CsvReader.class);
 
-    private String filename;
     private CsvLineProcessor csvLineProcessor;
 
-    public CsvReader(String filename, CsvLineProcessor csvLineProcessor) {
-        this.filename = filename;
+    public CsvReader( CsvLineProcessor csvLineProcessor) {
         this.csvLineProcessor = csvLineProcessor;
 
     }
 
-    public void readFile() throws Exception {
+    public void readFile(String filename) throws Exception {
         String[][] lines = CSVParser.parse(new FileReader(filename), ';');
         for (String[] line : lines) {
             csvLineProcessor.processLine(line);

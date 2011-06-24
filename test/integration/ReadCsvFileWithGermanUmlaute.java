927e3d0551b16608bc115a40e39b785235fafbd7
jhamel <johannes.hamel@gmail.com>
2011-06-24 17:28:54 +0200

    Clean TestCase to underline intention

--- test/integration/ReadCsvFileWithGermanUmlaute.java
+++ test/integration/ReadCsvFileWithGermanUmlaute.java
@@ -7,20 +7,15 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-
 public class ReadCsvFileWithGermanUmlaute {
     @Test
-    public void readUmlaute() {
+    public void readUmlaute() throws Exception {
 
         CsvReader csvReader = new CsvReader(new CsvLineProcessor() {
             public void processLine(String[] line) {
                 assertThat(line[0], equalTo("äbc"));
             }
         });
-        try {
-            csvReader.readFile(FixtureConstants.CSV_FILE_WITH_GERMAN_UMLAUTE);
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        csvReader.readFile(FixtureConstants.CSV_FILE_WITH_GERMAN_UMLAUTE);
     }
 }

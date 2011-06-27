2ac91cd30706ae82f12d5cdc1ef0e889ba08415a
jhamel <johannes.hamel@gmail.com>
2011-06-27 18:03:50 +0200

    Changes

--- test/integration/XlfTranslatorTest.java
+++ test/integration/XlfTranslatorTest.java
@@ -9,7 +9,6 @@ public class XlfTranslatorTest {
     private static final String BASEDIR = "C:\\Dokumente und Einstellungen\\J19727\\.dtc\\0\\DCs\\eonis.com\\eea\\bpexd\\wd";
     public static final String CSV = "C:/Dokumente und Einstellungen/J19727/Desktop/jhamel-WebDynpro-Translator-126e5a2/translation_20110330_DE-EN-FR-NL_VDE.csv";
 
-    @Test
     public void translate() {
         XlfTranslator xlfTranslator = new XlfTranslator(BASEDIR);
         xlfTranslator.translate(CSV, Locale.FRENCH, 0, 4);
